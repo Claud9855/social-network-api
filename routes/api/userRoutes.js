af69c469ae054f7e12ea433acde8973a2d223d1a
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-11-07 18:29:23 -0500

    implemented thought and user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+router.route('/').get(getUser).post(createUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteFriend);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
